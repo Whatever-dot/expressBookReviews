89fa2dfc0b4e18e817985057d9141c52c312ee44
Whatever-dot <jnamg1998@gmail.com>
2024-03-12 07:15:05 -0400

    changes made in theia lab environment

--- final_project/router/auth_users.js
+++ final_project/router/auth_users.js
@@ -9,20 +9,67 @@ const isValid = (username)=>{ //returns boolean
 //write code to check is the username is valid
 }
 
-const authenticatedUser = (username,password)=>{ //returns boolean
-//write code to check if username and password match the one we have in records.
+const authenticatedUser = (username,password)=>{
+    let validusers = users.filter((user)=>{
+      return (user.username === username && user.password === password)
+    });
+    if(validusers.length > 0){
+      return true;
+    } else {
+      return false;
+    }
 }
 
 //only registered users can login
 regd_users.post("/login", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (!username || !password) {
+        return res.status(404).json({message: "Error logging in"});
+    }
+  
+    if (authenticatedUser(username,password)) {
+      let accessToken = jwt.sign({
+        data: password
+      }, 'access', { expiresIn: 60 * 60 });
+  
+      req.session.authorization = {
+        accessToken,username
+    }
+    return res.status(200).send("User successfully logged in");
+    } else {
+      return res.status(208).json({message: "Invalid Login. Check username and password"});
+    }  
 });
 
 // Add a book review
 regd_users.put("/auth/review/:isbn", (req, res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    let username = req.session.authorization.username;
+    let book = books[isbn]
+    if (book) { //Check if book exists
+        let review = req.query.review;
+        
+        if(review) {
+            book["review"] = review
+        } 
+        
+        books[isbn] = book;
+        res.send(`Username ${username}'s review of ${book.title} updated.`);
+    } else{
+        res.send("Unable to find book!");
+    }
+});
+
+regd_users.delete("/auth/review/:isbn", (req, res) => {
+    const isbn = req.params.isbn;
+    let username = req.session.authorization.username;
+    let book = books[isbn]
+    if (isbn){
+        delete books[isbn]
+    }
+    res.send(`Username ${username} review of ${book.title} deleted.`);
 });
 
 module.exports.authenticated = regd_users;
--- final_project/router/general.js
+++ final_project/router/general.js
@@ -4,40 +4,107 @@ let isValid = require("./auth_users.js").isValid;
 let users = require("./auth_users.js").users;
 const public_users = express.Router();
 
+const axios = require('axios').default;
+
+const doesExist = (username)=>{
+    let users_with_same_name = users.filter((user)=>{
+      return user.username === username
+    });
+    if(users_with_same_name.length > 0){
+      return true;
+    } else {
+      return false;
+    }
+}  
 
 public_users.post("/register", (req,res) => {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const username = req.body.username;
+    const password = req.body.password;
+  
+    if (username && password) {
+      if (!doesExist(username)) { 
+        users.push({"username":username,"password":password});
+        return res.status(200).json({message: "User successfully registred. Now you can login"});
+      } else {
+        return res.status(404).json({message: "User already exists!"});    
+      }
+    } 
+    return res.status(404).json({message: "Unable to register user."});  
 });
 
 // Get the book list available in the shop
-public_users.get('/',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+const promise1 = new Promise((resolve, reject) => {
+    try {
+        public_users.get('/',function (req, res) {
+            resolve(res.send(JSON.stringify(books,null,4)));
+        });
+    } catch (err) {
+        reject(err)
+    }
 });
 
 // Get book details based on ISBN
 public_users.get('/isbn/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn])
  });
-  
-// Get book details based on author
-public_users.get('/author/:author',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
 
-// Get all books based on title
-public_users.get('/title/:title',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
-});
+// Get book details based on author
+const connectToURL = (url)=>{
+    const req = axios.get(url);
+    req.then(resp => {
+        public_users.get('/author/:author',function (req, res) {
+            let filtered_authors = [];
+            var keys = Object.keys(books);
+            let i = 0;
+            let j = 0
+            while(i < keys.length) {
+                if(books[keys[i]].author === req.params.author) {
+                    filtered_authors[j] = books[keys[i]];
+                    j++;
+                }
+                i++;
+            }
+            res.send(filtered_authors)
+        });
+    })
+    .catch(err => {
+        console.log(err.toString())
+    });
+}
+
+connectToURL('https://jnamg1998-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/')
+
+// Get book details based on title
+const connectToURL1 = (url)=>{
+    const req = axios.get(url);
+    req.then(resp => {
+        public_users.get('/title/:title',function (req, res) {
+            let filtered_titles = [];
+            var keys = Object.keys(books);
+            let i = 0;
+            let j = 0
+            while(i < keys.length) {
+                if(books[keys[i]].title === req.params.title) {
+                    filtered_titles[j] = books[keys[i]];
+                    j++;
+                }
+                i++;
+            }
+            res.send(filtered_titles)
+        });
+    })
+    .catch(err => {
+        console.log(err.toString())
+    });
+}
+
+connectToURL1('https://jnamg1998-5000.theianext-1-labs-prod-misc-tools-us-east-0.proxy.cognitiveclass.ai/')
 
 //  Get book review
 public_users.get('/review/:isbn',function (req, res) {
-  //Write your code here
-  return res.status(300).json({message: "Yet to be implemented"});
+    const isbn = req.params.isbn;
+    res.send(books[isbn].reviews)
 });
 
 module.exports.general = public_users;
